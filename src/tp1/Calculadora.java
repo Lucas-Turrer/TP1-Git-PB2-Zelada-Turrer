79fcd367c17db112607cd1eb7c443a800e49d6b0
Lucas-Turrer <63521780+Lucas-Turrer@users.noreply.github.com>
2020-04-19 19:02:34 -0300

    Modificacion de orden de las operaciones

--- src/tp1/Calculadora.java
+++ src/tp1/Calculadora.java
@@ -1,3 +1,4 @@
+package tp1;
 
 import java.util.Scanner;
 public class Calculadora {
@@ -8,7 +9,7 @@ public class Calculadora {
  Integer opcion;
  Integer resultado = 0;
  do{
- System.out.println("Ingrese su operación /n 1 para sumar /n 2 para\restar /n 3 para multiplicar /n 4 para dividir");
+ System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para\restar /n 3 para sumar /n 4 para dividir");
  opcion = teclado.nextInt();
  }while(opcion < 1 && opcion > 4);
  System.out.println("Ingrese el primer numero");
@@ -16,13 +17,13 @@ public class Calculadora {
  System.out.println("Ingrese el segundo numero");
  b = teclado.nextInt();
 switch(opcion) {
-case 1:
+case 3:
 	resultado = a+b;
 	break;
 case 2:
 	resultado = a-b;
 	break;
-case 3:
+case 1:
 	resultado = a*b;
 	break;
 case 4:
